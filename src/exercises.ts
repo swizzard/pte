f6c93ae6e80a0a09dd18842ebf5aeaef0b33d9ad
Sam Raker <sam.raker@gmail.com>
2024-10-05 11:41:24 -0400

    Update exercises.ts
    Remove 3-way kick, increase other reps

--- src/exercises.ts
+++ src/exercises.ts
@@ -6,12 +6,10 @@ const exercises: Exercises = [
   { label: "Piriformis Right", reps: 3 },
   { label: "Piriformis Left", reps: 3 },
   { label: "Butterfly", reps: 3 },
-  { label: "3-Way Kick Right", reps: 5 },
-  { label: "3-Way Kick Left", reps: 5 },
-  { label: "Marching w/Resistance", reps: 5 },
-  { label: "Squats", reps: 5 },
-  { label: "Clamshell Right", reps: 5 },
-  { label: "Clamshell Left", reps: 5 },
+  { label: "Marching w/Resistance", reps: 6 },
+  { label: "Squats", reps: 6 },
+  { label: "Clamshell Right", reps:6 },
+  { label: "Clamshell Left", reps: 6 },
 ];
 
 export default exercises;
