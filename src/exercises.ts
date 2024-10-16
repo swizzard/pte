c23b96aaa5ddd4c4dfbb221ea4b977609ec07a03
Sam Raker <sam.raker@gmail.com>
2024-10-16 14:13:48 -0400

    Update exercises.ts
    increase all reps by 1

--- src/exercises.ts
+++ src/exercises.ts
@@ -1,15 +1,15 @@
 import { type Exercises } from "./reducer";
 
 const exercises: Exercises = [
-  { label: "ITB Right", reps: 3 },
-  { label: "ITB Left", reps: 3 },
-  { label: "Piriformis Right", reps: 3 },
-  { label: "Piriformis Left", reps: 3 },
-  { label: "Butterfly", reps: 3 },
-  { label: "Marching w/Resistance", reps: 6 },
-  { label: "Squats", reps: 6 },
-  { label: "Clamshell Right", reps:6 },
-  { label: "Clamshell Left", reps: 6 },
+  { label: "ITB Right", reps: 4 },
+  { label: "ITB Left", reps: 4 },
+  { label: "Piriformis Right", reps: 4 },
+  { label: "Piriformis Left", reps: 4 },
+  { label: "Butterfly", reps: 4 },
+  { label: "Marching w/Resistance", reps: 7 },
+  { label: "Squats", reps: 7 },
+  { label: "Clamshell Right", reps: 7 },
+  { label: "Clamshell Left", reps: 7 },
 ];
 
 export default exercises;
