fd17dd9dc38cf6ced9f04a2a178dc6042d065285
Sam Raker <sam.raker@gmail.com>
2024-11-10 16:08:41 -0500

    increase reps to 8

--- src/exercises.ts
+++ src/exercises.ts
@@ -6,10 +6,10 @@ const exercises: Exercises = [
   { label: "Piriformis Right", reps: 4 },
   { label: "Piriformis Left", reps: 4 },
   { label: "Butterfly", reps: 4 },
-  { label: "Marching w/Resistance", reps: 7 },
-  { label: "Squats", reps: 7 },
-  { label: "Clamshell Right", reps: 7 },
-  { label: "Clamshell Left", reps: 7 },
+  { label: "Marching w/Resistance", reps: 8 },
+  { label: "Squats", reps: 8 },
+  { label: "Clamshell Right", reps: 8 },
+  { label: "Clamshell Left", reps: 8 },
 ];
 
 export default exercises;
